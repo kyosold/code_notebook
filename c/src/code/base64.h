1cfbd0f34b93587297946206f156374007572556
kyosold <kyosold@qq.com>
2021-06-03 15:53:01 +0800

    修复base64函数名

--- c/src/code/base64.h
+++ c/src/code/base64.h
@@ -1,7 +1,7 @@
 #ifndef _S_BASE64_H
 #define _S_BASE64_H
 
-unsigned char *sbase64_encode_alloc(const unsigned char *, int, int *);
-unsigned char *sbase64_decode_alloc(const unsigned char *, int, int *);
+unsigned char *base64_encode_alloc(const unsigned char *, int, int *);
+unsigned char *base64_decode_alloc(const unsigned char *, int, int *);
 
 #endif
